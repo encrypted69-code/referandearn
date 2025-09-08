38f0ba1f98a031024d89bef831348f8d2f010ec6
encrypted69-code <iamgreedy6@gmail.com>
2025-09-08 12:53:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://encryptedyt077_db_user:4iG7om8ELQJmzvGm@earnify.spbk6jd.mongodb.net/?retryWrites=true&w=majority&appName=earnify")
 BOT_TOKEN = BOT_TOKEN = "8400875138:AAE2zrn4ovvwAj98NPbuXALsPm3rSiRtRIk"
+ADMIN_IDS = [7119001414]  # List of admin Telegram user IDs
 LOGGER_ID = 7119001414  # Telegram ID for logging messages
 FSUB_IDS = [-1002661417456]  # Telegram channel/group IDs for forwarding/subscription
 MIN_WITHDRAWAL_AMOUNT = 100  # Minimum points required to withdraw
