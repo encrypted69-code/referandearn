8c73c4cf8f8bd35498de530ec1ca382d391340a1
encrypted69-code <iamgreedy6@gmail.com>
2025-09-08 12:43:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://encryptedyt077_db_user:4iG7om8ELQJmzvGm@earnify.spbk6jd.mongodb.net/?retryWrites=true&w=majority&appName=earnify")
-BOT_TOKEN = os.getenv("TOKEN", "8400875138:AAE2zrn4ovvwAj98NPbuXALsPm3rSiRtRIk")
+MONGO_URI = os.getenv "mongodb+srv://encryptedyt077_db_user:4iG7om8ELQJmzvGm@earnify.spbk6jd.mongodb.net/?retryWrites=true&w=majority&appName=earnify"
+BOT_TOKEN = os.getenv "8400875138:AAE2zrn4ovvwAj98NPbuXALsPm3rSiRtRIk"
 ADMIN_IDS = [7119001414]  # Owner and admins (Telegram user IDs)
 LOGGER_ID = 7119001414  # Telegram ID for logging messages
 FSUB_IDS = [-1002661417456]  # Telegram channel/group IDs for forwarding/subscription
